413be8832a739f5784c417b00f40028374f8b581
usernameAIo <100479515+usernameAIo@users.noreply.github.com>
2022-03-19 17:07:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,73 @@
+import pygame, sys
+
+level_map = [
+'XXXXXXXXXXXXXXXXXXXXXXX',
+'X                     X',
+'X  P XXX              X',
+'XXXXXX     XX         X',
+'X               XXXXXXX',
+'X     XX              X',
+'XXXXXXX               X',
+'X                 XXXXX',
+'X               XX    X',
+'X                     X',
+'XXXXXXXXXXXXXXXXXXXXXXX']
+
+tile_size = 64
+
+pygame.init()
+screen_width, screen_height = 1200, len(level_map) * tile_size
+FPS = 60
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.Surface((32, 64))
+        self.image.fill('red')
+        self.rect = self.image.get_rect(topleft=pos)
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, pos, size):
+        super().__init__()
+        self.image = pygame.Surface((size, size))
+        self.image.fill('ORANGE')
+        self.rect = self.image.get_rect(topleft=pos)
+    def update(self, x_shift):
+        self.rect.x += x_shift
+class Level:
+    def __init__(self, level_data, surface):
+        self.display_surface = surface
+        self.setup_level(level_data)
+        self.world_shift = 0
+    def setup_level(self, layout):
+        self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
+        for row_index, row in enumerate(layout):
+            for col_index, cell in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
+                if cell == 'X':
+                    tile = Tile((x, y), tile_size)
+                    self.tiles.add(tile)
+                if cell == 'P':
+                    player_sprite = Player((x, y))
+                    self.player.add(player_sprite)
+    def run(self):
+        self.tiles.update(self.world_shift)
+        self.tiles.draw(self.display_surface)
+        self.player.draw(self.display_surface)
+
+level = Level(level_map, screen)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill('black')
+    level.run()
+    pygame.display.update()
+    clock.tick(FPS)
