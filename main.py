5f15ce35ac6e52c692850ba454ebdd52c9cdc9d6
usernameAIo <100479515+usernameAIo@users.noreply.github.com>
2022-03-19 17:33:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pygame, sys
 level_map = [
 'XXXXXXXXXXXXXXXXXXXXXXX',
 'X                     X',
-'X  P XXX              X',
+'X  P                  X',
 'XXXXXX     XX         X',
 'X               XXXXXXX',
-'X     XX              X',
+'X      X              X',
 'XXXXXXX               X',
 'X                 XXXXX',
 'X               XX    X',
@@ -24,9 +24,22 @@ clock = pygame.time.Clock()
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        self.image = pygame.Surface((32, 64))
+        self.image = pygame.Surface((40, 64))
         self.image.fill('red')
         self.rect = self.image.get_rect(topleft=pos)
+        self.direction = pygame.math.Vector2(0, 0)
+        self.speed = 8
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+    def update(self):
+        self.get_input()
+        self.rect.x += self.direction.x * self.speed
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, size):
@@ -57,6 +70,7 @@ class Level:
     def run(self):
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+        self.player.update()
         self.player.draw(self.display_surface)
 
 level = Level(level_map, screen)
